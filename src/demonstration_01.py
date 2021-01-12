e47e8d940f6a1c2fc5a3ecd346f7901b222c798d
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2021-01-12 11:02:44 -0800

    problem-solving

--- src/demonstration_01.py
+++ src/demonstration_01.py
@@ -7,14 +7,52 @@ Examples:
 - last([1, 2, 3, 4, 5], 1) ➞ [5]
 - last([4, 3, 9, 9, 7, 6], 3) ➞ [9, 7, 6]
 - last([1, 2, 3, 4, 5], 7) ➞ "invalid"
-- last([1, 2, 3, 4, 5], 0) ➞ []
+- last([1, 2, 3, 4, 5], 7) ➞ []
 
 Notes:
 - Return "invalid" if n exceeds the length of the list.
 - Return an empty list if n == 0.
+    
+What if n is:
+    + a string -- if it's not an int, force to int with int()
+    + a float -- if its not an int, force to int with int()
+    + negative -- return empty list
+    + complex number?  -- don't need to deal with this
+
+What if the list 
+    + is empty
 """
 
 
 def last(a, n):
-    # Your code here
+    # Your code here                                 
+    # Make sure n is an int
+    try:
+        n = int(n)
+    except ValueError:
+        return "invalid number"
+    #  Make sure n isn't too large
+    if n > len(a):
+        return "invalid"
+    # Make sure n is positive
+    if n == 0:
+        return []
+    # # Check if n is 0 
+    # if n == 0 
+    #     return []
+    # Get the last elements from the list  
+    result = []
+    # Loop n times
+    for _ in range(n):
+        # pop the value off the end of the list 
+        val = a.pop()
+        # insert the value at the front of the result list 
+        result.insert(0, val)
 
+    return result
+
+print(last([4, 3, 9, 9, 7, 6], 3))
+print(last([1, 2, 3, 4, 5], 7))
+print(last([1, 2, 3, 4, 5], -7))
+print(last([1, 2, 3, 4, 5], 7), 'beej')
+         
--- src/demonstration_07.py
+++ src/demonstration_07.py
@@ -11,7 +11,41 @@ Examples:
 - repeat_it("abcd") -> "A-Bb-Ccc-Dddd"
 - repeat_it("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 - repeat_it("cwAt") -> "C-Ww-Aaa-Tttt"
+
+Questions: 
+    + What id there are non-alphabetic characters in the string? Including space?
+        + We don't need to worry about this
+    + What if we get an empty string? 
+        + Return an empty string 
 """
 def repeat_it(input_str):
-    # Your code here
+    #  Check for empty string
+    if input_str == "":
+        return ""
+    # Convert the input string to a list 
+    input_list = list(input_str)
+    result_list = []
+    # Loop through the list 
+    for reps, char in enumerate(input_list):
+        repeated_char = ""    
 
+        #  Repeat the character
+        for _ in range(reps):
+            repeated_char +=char
+
+        #  Capitalize the new strings
+        repeated_char = repeated_char.capitalize()
+        print(repeated_char)
+        #  Add items to result list
+        result_list.append(repeated_char)
+    print(result_list)
+    # Join result list with - 
+    result_str = "-".join(result_list)
+    # Return the string  
+    return result_str
+print(repeat_it("abcd"))
+print("-- First demo --")
+print(repeat_it("RqaEzty"))
+print("-- Second demo --")
+print(repeat_it("cwAt"))
+print("-- Third demo --")
